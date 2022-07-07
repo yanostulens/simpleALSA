5cca13b9092e4b31ce1ff27d610c94fce6f1dc39
Yano <yano.stulens@live.be>
2022-07-07 17:34:27 +0200

    Added function definitions for the simple ALSA device

--- simpleALSA.h
+++ simpleALSA.h
@@ -2,14 +2,35 @@
 
 /** ENUMS **/
 
+/**
+ * @brief enum used to return function results
+ * 
+ */
 enum sa_result {
     SA_SUCCESS = 0,
     SA_ERROR = 1
 }
 
+/**
+ * @brief enum used to indicate the status of a device
+ * 
+ */
+enum sa_device_status {
+    UNINITIALIZED = 0,
+    INITIALIZED = 1,
+    STARTED = 2,
+}
+
+
+
+
 
 /** STRUCTS **/
 
+/**
+ * @brief struct used to config a simple ALSA devicre
+ * 
+ */
 struct sa_device_config
 {
     int sampleRate; /** Rate at which samples are send through the soundcard */
@@ -23,20 +44,56 @@ struct sa_device_config
     void (*callbackFunction)(int framesToSend, void* audioBuffer, sa_device* sa_device);   /** Callback function that will be called whenever the internal buffer is running empty and new audio samples are required */
 };
 
+/**
+ * @brief struct used to encapsulate a simple ALSA device
+ * 
+ */
+struct sa_device
+{
+    sa_device_config* config;
+    sa_device_status status;
+};
+
+
+
 
 /** FUNCTIONS DEFINITIONS **/
 
 /**
  * @brief creates an sa_device_config struct and sets it to default values
- * @param sa_device_config* device - empty pointer into which the config struct is set
+ * @param device - empty pointer into which the config struct is set
  * @return sa_return_status
  */
 sa_return_status sa_init_device_config(sa_device_config* config);
 
 /**
  * @brief initializes a new audio device
- * @param sa_device_config* config - configuration used to initialize the device
- * @param sa_device* device - pointer to the initialized audio device
+ * @param config - configuration used to initialize the device
+ * @param device - pointer to the initialized audio device
  * @return sa_return_status 
  */
 sa_return_status sa_init_device(sa_device_config* config, sa_device* device);
+
+/**
+ * @brief starts the simple ALSA device - which starts the callback loop
+ * 
+ * @param device - device to start 
+ * @return sa_return_status 
+ */
+sa_return_status sa_start_device(sa_device* device);
+
+/**
+ * @brief pauses the simple ALSA device - which pauses the callback loop
+ * 
+ * @param device - device to pause
+ * @return sa_return_status 
+ */
+sa_return_status sa_pause_device(sa_device* device);
+
+/**
+ * @brief destroys the device - device pointer is set to NULL
+ * 
+ * @param device - the device to destroy
+ * @return sa_return_status 
+ */
+sa_return_status sa_destroy_device(sa_device* device);
