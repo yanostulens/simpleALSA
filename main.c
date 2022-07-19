23606bb9f4818bace4846905073c804932e3afc8
Yano <yano.stulens@live.be>
2022-07-19 12:10:48 +0200

    Delete GenerateSine functions

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ static snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;    /* sample format */
 static unsigned int rate = 48000;           /* stream rate */
 static unsigned int channels = 2;           /* count of channels */
 static unsigned int buffer_time = 500000;       /* ring buffer length in us */
-static unsigned int period_time = 100000;       /* period time in us */
+static unsigned int period_time = 250000;       /* period time in us */
 static double freq = 300;               /* sinusoidal wave frequency in Hz */
 static int resample = 1;                /* enable alsa-lib resampling */
 static int period_event = 0;                /* produce poll event after each period */
@@ -16,7 +16,7 @@ static int period_event = 0;                /* produce poll event after each per
 static snd_pcm_sframes_t buffer_size;
 static snd_pcm_sframes_t period_size;
 
-char *infilename = "./california.wav";
+char *infilename = "./audioFiles/california.wav";
 SF_INFO sfinfo;
 SNDFILE *infile = NULL;
 
@@ -30,73 +30,6 @@ static void initSndFile() {
     fprintf(stderr,"Sample rate: %d\n", sfinfo.samplerate);
     fprintf(stderr,"Sections: %d\n", sfinfo.sections);
     fprintf(stderr,"Format: %d\n", sfinfo.format);
-}
-
-static void generate_sine(const snd_pcm_channel_area_t *areas, 
-              snd_pcm_uframes_t offset,
-              int count, double *_phase)
-{
-    static double max_phase = 2. * M_PI;
-    double phase = *_phase;
-    double step = max_phase*freq/(double)rate;
-    unsigned char *samples[channels];
-    int steps[channels];
-    unsigned int chn;
-    int format_bits = snd_pcm_format_width(format);
-    unsigned int maxval = (1 << (format_bits - 1)) - 1;
-    int bps = format_bits / 8;  /* bytes per sample */
-    int phys_bps = snd_pcm_format_physical_width(format) / 8;
-    int big_endian = snd_pcm_format_big_endian(format) == 1;
-    int to_unsigned = snd_pcm_format_unsigned(format) == 1;
-    int is_float = (format == SND_PCM_FORMAT_FLOAT_LE ||
-            format == SND_PCM_FORMAT_FLOAT_BE);
- 
-    /* verify and prepare the contents of areas */
-    for (chn = 0; chn < channels; chn++) {
-        if ((areas[chn].first % 8) != 0) {
-            printf("areas[%u].first == %u, aborting...\n", chn, areas[chn].first);
-            exit(EXIT_FAILURE);
-        }
-        samples[chn] = /*(signed short *)*/(((unsigned char *)areas[chn].addr) + (areas[chn].first / 8));
-        if ((areas[chn].step % 16) != 0) {
-            printf("areas[%u].step == %u, aborting...\n", chn, areas[chn].step);
-            exit(EXIT_FAILURE);
-        }
-        steps[chn] = areas[chn].step / 8;
-        samples[chn] += offset * steps[chn];
-    }
-
-    /* fill the channel areas */
-    while (count-- > 0) {
-        union {
-            float f;
-            int i;
-        } fval;
-        
-        int res, i;
-        if (is_float) {
-            fval.f = sin(phase);
-            res = fval.i;
-        } else
-            res = sin(phase) * maxval;
-        if (to_unsigned)
-            res ^= 1U << (format_bits - 1);
-        for (chn = 0; chn < channels; chn++) {
-            /* Generate data in native endian format */
-            if (big_endian) {
-                for (i = 0; i < bps; i++)
-                    *(samples[chn] + phys_bps - 1 - i) = (res >> i * 8) & 0xff;
-            } else {
-                for (i = 0; i < bps; i++)
-                    *(samples[chn] + i) = (res >>  i * 8) & 0xff;
-            }
-            samples[chn] += steps[chn];
-        }
-        phase += step;
-        if (phase >= max_phase)
-            phase -= max_phase;
-    }
-    *_phase = phase;
 }
 
 static int set_hwparams(snd_pcm_t *handle, snd_pcm_hw_params_t *params, snd_pcm_access_t access)
