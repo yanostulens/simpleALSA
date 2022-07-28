4cdcc273ffbe1533abef5a38b29dc5ba21fc1b9c
yano <yano@pvs.global>
2022-07-28 15:10:52 +0200

    Update on logger

--- simpleALSA.h
+++ simpleALSA.h
@@ -531,17 +531,17 @@ static void sa_log(sa_log_type type, const char msg0[], const char msg1[]) {
     {
     #ifndef SA_NO_ERROR_LOGS
     case ERROR:
-        printf("\e[1;31m[  ERROR   ] \e[0m %s %s\n", msg0, msg1);
+        printf("\e[1;31m[  SA ERROR   ] \e[0m %s %s\n", msg0, msg1);
         break;
     #endif
     #ifndef SA_NO_WARNING_LOGS
     case WARNING:
-        printf("\e[1;33m[  WARNING ] \e[0m %s %s\n", msg0, msg1);
+        printf("\e[1;33m[  SA WARNING ] \e[0m %s %s\n", msg0, msg1);
         break;
     #endif
     #ifndef SA_NO_DEBUG_LOGS
     case DEBUG:
-        printf("\e[1;35m[  DEBUG   ] \e[0m %s %s\n", msg0, msg1);
+        printf("\e[1;35m[  SA DEBUG   ] \e[0m %s %s\n", msg0, msg1);
         break;
     #endif
     default:
